18e26b2db130076d9a4c73a0780f8cec3d982496
Martina <martinacamilaleon@gmail.com>
2025-09-23 15:04:01 -0500

    reto 3 .env

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,19 @@
+require('dotenv').config();
 const express = require('express'); 
 const app = express();
+const port = process.env.PORT || 5000;
+const jwtSecret = process.env.JWT_SECRET;
+
 
 app.use(express.json());
+console.log("secret password: ",jwtSecret)
+console.log("port: ",port)
 
 app.get('/', (req, res) => {
     res.send('API backend is running...');
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
