c6e00d7dd667efa6af38101a560ffd08be4a7c6d
Martina <martinacamilaleon@gmail.com>
2025-09-24 22:08:57 -0500

    bbdd initial structure

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const jwtSecret = process.env.JWT_SECRET;
+const tareaRoutes = require('./routes/tareaRoutes');
 
 
 app.use(express.json());
@@ -13,6 +14,7 @@ app.get('/', (req, res) => {
     res.send('API backend is running...');
 });
 
+app.use('/api',tareaRoutes) /// /api/tareas
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
---
+++ backend/src/routes/tareaRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const tareaController = require('../controller/tareaController');   
+
+router.get('/tareas', tareaController.getTareas);
+
+module.exports = router;
+  /// /api/tareas
