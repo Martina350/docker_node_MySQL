357748276bbf745c6e86a5c3e5d00118c88b5656
Martina <martinacamilaleon@gmail.com>
2025-09-27 21:52:44 -0500

    documentation with scalar //error

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,12 @@ const jwtSecret = process.env.JWT_SECRET;
 const tareaRoutes = require('./routes/tareaRoutes');
 const authRoutes = require('./routes/authRoutes');
 const cors = require('cors');
+const path = require('path');
+const { apiReference } = require('@scalar/express-api-reference')
 
 const corsOption = {
-    origin: 'http://localhost:5000' || process.env.CORS_ORIGIN
+    origin: 'http://localhost:5000' || process.env.CORS_ORIGIN,
+    credentials: true,
 }
 
 app.use(express.json());
@@ -22,6 +25,32 @@ app.get('/', (req, res) => {
 app.use('/api',tareaRoutes) /// /api/tareas
 app.use('/api', authRoutes);
 
+app.use('/docs', apiReference({
+    theme: 'dark',
+    layout: 'modern',
+    spec: {
+        url: '/api/openapi.yaml',
+    },
+    configuration: {
+        showSidebar: true,
+        hideDownloadButton: false,
+        hideTryItPanel: false,
+        authentication: {
+            preferredSecurityScheme: "bearerAuth",
+            apikey: {
+                token: 'token'
+            }
+        }
+    }
+}));
+
+app.get('/api/openapi.yaml', (req, res) => {
+    res.setHeader('Content-Type', 'application/x-yaml');
+    res.sendFile(path.join(__dirname, '../docs/openapi.yaml'));
+});
+    
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+    console.log(`API documentation available at http://localhost:${port}/docs`);
 });
